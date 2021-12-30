c5fa62b650196f1c0c6d91d8964c432ec56e1cfa
Arthur HUGEAT <arthur.hugeat@jet1oeil.com>
2021-12-30 14:57:18 +0100

    Iterate over VMAF limit

--- tools/glpk-solver/main.cpp
+++ tools/glpk-solver/main.cpp
@@ -1,10 +1,11 @@
 #include <cassert>
 #include <iostream>
+#include <fstream>
 
 #include "local/Data.h"
 #include "local/LinearSolver.h"
 
-bool solveLinearProgram(const local::ExperimentResults& exps, double vmafLimit, double maxTime)
+bool solveLinearProgram(const local::ExperimentResults& exps, double vmafLimit, double maxTime, std::ofstream& solFile, std::ofstream& dataFile)
 {
 	local::LinearSolver solver(vmafLimit, maxTime);
 	solver.fillContraints(exps);
@@ -14,12 +15,18 @@ bool solveLinearProgram(const local::ExperimentResults& exps, double vmafLimit, 
 		return false;
 	}
 
-	std::cout << "Selected config: " << std::endl;
-	std::cout << "\t- codec: " << exps[iResultIndex].config.szCodecName << std::endl;
-	std::cout << "\t- preset: " << exps[iResultIndex].config.szPreset << std::endl;
-	std::cout << "\t- crf: " << exps[iResultIndex].config.iCRF << std::endl;
-	std::cout << "\t- vmaf: " << exps[iResultIndex].result.dVMAF << std::endl;
-	std::cout << "\t- encoding time: " << exps[iResultIndex].result.iEncodingTime << " ms" << std::endl;
+	solFile << vmafLimit;
+	solFile << "\t\t" << maxTime;
+	solFile << "\t\t" << exps[iResultIndex].config.szCodecName;
+	solFile << "\t"<< exps[iResultIndex].config.szPreset;
+	solFile << "\t" << exps[iResultIndex].config.iCRF;
+	solFile << "\t" << exps[iResultIndex].result.dVMAF;
+	solFile << "\t" << exps[iResultIndex].result.iEncodingTime << std::endl;
+
+	dataFile << vmafLimit;
+	dataFile << "\t" << exps[iResultIndex].result.iEncodingTime;
+	dataFile << "\t" << exps[iResultIndex].result.iBitstreamSize;
+	dataFile << "\t" << exps[iResultIndex].result.dVMAF << std::endl;
 
 	return true;
 }
@@ -37,8 +44,16 @@ int main(int argc, char* argv[])
 	auto exps = local::loadExperimentResults(argv[1]);
 	assert(exps.size() > 0);
 
+	// Iterate over VMAF limit
+	std::ofstream vmafSolFile("vmaf-time-1.0.sol", std::ios::out);
+	assert(vmafSolFile.good());
+	vmafSolFile << "vmaf limit\tmax time\tcodec\t\tpreset\t\tcrf\tvmaf\tencoding time" << std::endl;
+
+	std::ofstream vmafDataFile("vmaf-time-1.0.dat", std::ios::out);
+	assert(vmafDataFile.good());
+
 	for (int i = 50; i < 100; ++i) {
-		solveLinearProgram(exps, i, std::stod(argv[2]));
+		solveLinearProgram(exps, i, std::stod(argv[2]), vmafSolFile, vmafDataFile);
 	}
 
 	return 0;
